16f0d1d3ada71c85f7408817cbf6b7ed91896ff3
JulianRuiz319 <146869143+JulianRuiz319@users.noreply.github.com>
2023-10-13 12:19:33 -0500

    Add files via upload

---
+++ m3 problem3.py
@@ -0,0 +1,20 @@
+#Julian Ruiz
+#10/13/23
+
+
+# # Get input from the users
+def greet_person(name):
+    return f"Hello, {name}! How are you today?"
+
+# Get input from the first person
+person1 = input("Enter the name of the first person: ")
+greeting1 = greet_person(person1)
+print(greeting1)
+
+# Get input from the second person
+person2 = input("Enter the name of the second person: ")
+greeting2 = greet_person(person2)
+print(greeting2)
+
+
+
