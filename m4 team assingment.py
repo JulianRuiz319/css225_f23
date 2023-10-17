8b0ef4e7be5273ef28485f36ab46a92a969b72e2
JulianRuiz319 <146869143+JulianRuiz319@users.noreply.github.com>
2023-10-17 11:48:19 -0500

    Add files via upload

---
+++ m4 team assingment.py
@@ -0,0 +1,18 @@
+#Julian Ruiz
+#10/17/23
+
+
+print("Choose an operation: ")
+print("1. Learn Python")
+print("2. Learn Java")
+print("3. Go swimming")
+print("4. Have dinner")
+print("5. Go to bed")
+print("0. Exit")
+
+
+choice = input("Enter 1 or 0 to choose the operation: ")
+
+# second way you could do it
+# print("chose an operator:")
+# print("1.learn python \n2.Learn Java \n3.Go swimming \n4.Have dinner \n5.Go to bed \n6.Exit")
