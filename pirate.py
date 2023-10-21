42f5bd6b043ab451553668034d3828b1b1ba162a
JulianRuiz319 <146869143+JulianRuiz319@users.noreply.github.com>
2023-10-21 11:49:39 -0500

    Add files via upload

---
+++ pirate.py
@@ -0,0 +1,8 @@
+#Debuged by Julian Ruiz
+#10/20/23
+
+greeting = input("Hello, possible pirate! What's the password?")
+if greeting in ["Arrr!"]:
+    print("Go away, pirate!")
+else:
+    print("Greetings, hater of pirates!")
